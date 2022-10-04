546ba5326978542700cd12077af2e0b96f3886fb
neww <@1786069549@qq.com>
2022-10-04 19:11:14 +0800

    请求工具-请求函数

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <div>
-    App
+  <div class="container">
+    App {{ $store.state.user.profile.account }}
+    <button @click="$store.commit('user/setUser', {account:'zhoushuyi'})">设置用户信息</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'App'
 }
 </script>
-
-<style>
-
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,21 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
 
-const moduleA = {
-  state:[username:'zs'],
-}
-
-const moduleB = {
-  state:[username:'zs'],
-}
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
 
 export default createStore({
   modules: {
-    moduleA,
-    moduleB
-  }
+    cart,
+    user,
+    category
+  },
+  // 配置插件
+  plugins: [createPersistedstate({
+    // 本地存储名字
+    key: 'erabbit-client-pc-store',
+    // 指定需要存储的模块
+    paths: ['user', 'cart']
+  })]
 })
-
