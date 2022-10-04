b37cb33bb0b2d0037daca26408140e74db5e7699
neww <@1786069549@qq.com>
2022-10-04 19:39:45 +0800

    请求工具-请求函数2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="container">
-    App {{ $store.state.user.profile.account }}
-    <button @click="$store.commit('user/setUser', {account:'zhoushuyi'})">设置用户信息</button>
+    App <button @click="fn">测试request工具函数</button>
   </div>
 </template>
 
 <script>
+import request from './utils/request'
 export default {
-  name: 'App'
+  name: 'App',
+  setup () {
+    const fn = () => {
+      request('/member/profile', 'get', { a : 10 })
+    }
+    return { fn }
+  }
 }
 </script>
