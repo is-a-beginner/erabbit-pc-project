c7e2c00ee514ed39bc951857a7d2dc30e8116587
neww <@1786069549@qq.com>
2022-10-07 22:55:13 +0800

    完成头部导航渲染 头部导航交互 吸顶头部组件

--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -1,10 +1,43 @@
 // 分类模块
+import { topCategory } from '@/api/constants'
+import { findAllCategory } from '@/api/category.js'
+
 export default {
   namespaced: true,
-  state () {
+  state: () => {
     return {
-      // 分类信息集合
-      list: []
+      // 分类信息集合 依赖topCategory重新设置 保证初始化就拿到数据
+      list: topCategory.map(item => ({ name:item }))
+    }
+  },
+  //修改分类函数
+  mutations: {
+    //payload 所有的分类集合
+    setList (state, payload) {
+      state.list = payload
+    },
+    show(state, id) {
+      const currCategory = state.list.find(item => item.id === id)
+      currCategory.open = true
+    },
+    hide(state, id) {
+      const currCategory = state.list.find(item => item.id === id)
+      currCategory.open = false
+    }
+  },
+  //获取分类函数
+  actions: {
+    async getList ({ commit }) {
+      // 获取分类数据
+      const data = await findAllCategory()
+      //给一级分类加上一个控制二级分类显示隐藏的数据open
+      data.result.forEach(top => {
+        top.open = false 
+      })
+      // 修改分类数据
+      // console.log(data);
+      commit('setList', data.result)
     }
   }
+
 }
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,7 +3,11 @@
   <AppTopNav></AppTopNav>
 
   <!-- 头部通栏 -->
-  <AppHeader></AppHeader>
+  <AppHeader>
+  </AppHeader>
+
+   <!-- 吸顶头部 -->
+   <AppHeaderSticky></AppHeaderSticky>
 
   <!-- 内容容器 -->
   <div class="main">
@@ -19,13 +23,20 @@
   import AppTopNav from '@/components/app-topnav.vue'
   import AppHeader from '@/components/app-header.vue'
   import AppFooter from '@/components/app-footer.vue'
+  import AppHeaderSticky from '@/components/app-header-sticky.vue'
+  import { useStore } from 'vuex'
 
 export default {
   name: 'Layout',
   components: {
     AppTopNav,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky
+  },
+  setup () {
+    const store = useStore()
+    store.dispatch('category/getList')
   }
 }
 </script>
